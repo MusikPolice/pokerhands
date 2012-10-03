ae3d9caae124cfca8344113a9840508efbbd7ecf
MusikPolice <jonfritz@gmail.com>
2012-10-02 23:31:46 -0400

    The game now deals two hands and a community, Texas Hold 'em rules

--- src/pokerhands/Pokerhands.java
+++ src/pokerhands/Pokerhands.java
@@ -1,5 +1,8 @@
 package pokerhands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author jfritz
@@ -14,9 +17,48 @@ public class Pokerhands
         Deck d = new Deck();
         d.shuffle();
         
-        for (Card c : d.drawCards(10))
+        List<Card> hand1 = new ArrayList<>();
+        List<Card> hand2 = new ArrayList<>();
+        List<Card> community = new ArrayList<>();
+        
+        //deal two hands
+        hand1.add(d.drawCard());
+        hand2.add(d.drawCard());
+        hand1.add(d.drawCard());
+        hand2.add(d.drawCard());
+
+        //deal the flop
+        d.drawCard();
+        community.addAll(d.drawCards(3));
+        
+        //deal the river
+        d.drawCard();
+        community.add(d.drawCard());
+        
+        //deal the turn
+        d.drawCard();
+        community.add(d.drawCard());
+        
+        
+        System.out.println("Player 1:");
+        for(Card c: hand1)
         {
             System.out.println(c.toString());
         }
+        System.out.println();
+        
+        System.out.println("Player 2:");
+        for(Card c: hand2)
+        {
+            System.out.println(c.toString());
+        }
+        System.out.println();
+        
+        System.out.println("Community:");
+        for(Card c: community)
+        {
+            System.out.println(c.toString());
+        }
+        System.out.println();
     }
 }
